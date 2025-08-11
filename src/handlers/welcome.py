d9a1aa823802c5c41f9e98949523431d42c0f3ff
Qularth <mikanen43@mail.ru>
2025-08-11 18:23:40 +0300

    registration added

--- src/handlers/welcome.py
+++ src/handlers/welcome.py
@@ -1,9 +1,21 @@
-from main import bot
+from src.common import bot
+from src.dao.models import AsyncSessionLocale, User
+
 
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
-    await bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
-    
+    async with AsyncSessionLocale() as session:
+        user = await session.get(User, message.from_user.id)
+        if not user:
+            user = User(
+                telegram_id=message.from_user.id,
+                username=message.from_user.username,
+                first_name=message.from_user.first_name,
+                last_name=message.from_user.last_name
+            )
+            session.add(user)
+            await session.commit()
+            await bot.reply_to(message, "Добро пожаловать! Вы зарегестрированы.")
+        else:
+            await bot.reply_to(message, "C Возвращением!")
+
