72d6d7d3d95189bd8ef003eb9e18d469106668fa
Qularth <mikanen43@mail.ru>
2025-08-11 19:25:06 +0300

    Добавлены условия для создания AsyncSessionLocal

--- src/dao/models.py
+++ src/dao/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime,create_engine
+from sqlalchemy import BigInteger, Column, Integer, String, Boolean, DateTime,create_engine
 from sqlalchemy.orm import declarative_base
 from datetime import datetime, UTC
 import os
@@ -10,14 +10,14 @@ Base = declarative_base()
 class User(Base): 
     __tablename__="Echofrost_Users"
  
-    telegram_id= Column(Integer, primary_key=True) #ID пользователя телеграм 
+    telegram_id= Column(BigInteger, primary_key=True) #ID пользователя телеграм 
     username=Column(String(50), nullable=True) # @username (None- если скрыт)
     first_name=Column(String(50),nullable=True) # Имя
     last_name=Column(String(50),nullable=True) # Фамилия (None- если нет)
     phone=Column(String(20),nullable=True) # Телефон (Eсли предоставит)
     is_admin=Column(Boolean, default=False) # Админ/нет
     is_active=Column(Boolean, default=False) # Активен ли аккаунт?
-    registrated_at= Column(DateTime, default=datetime.now(tz=UTC)) #Дата регистрации
+    registrated_at= Column(DateTime(timezone=True), default=datetime.now(tz=UTC)) #Дата регистрации
 
     def __repr__(self):
         return f"<User (id{self.telegram_id}, username='{self.username})'>"
@@ -31,4 +31,4 @@ if ECHOFROST_ASYNC_DATABASE_URL := os.getenv("ECHOFROST_ASYNC_DATABASE_URL"):
         os.getenv("ECHOFROST_ASYNC_DATABASE_URL",'sqlite+aiosqlite:///EchoFrost.db'), 
         echo=True)  
    
-AsyncSessionLocale= sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False) #NoQa
+    AsyncSessionLocale= sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False) #NoQa
